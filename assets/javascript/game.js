fc5fb0a2e4c9046b4e3cfaad61335fc893833ba5
prpatel <prpatel1231@gmail.com>
2019-11-23 08:40:42 -0500

    added initialized variable and functions needed in game

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// array of letters to guess
+var letters = ["a","b","c","d","e","f","g",
+                    "h","i","j","k","l","m","n","o",
+                    "p","q","r","s","t","u","v","w",
+                    "x","y","z"];
+
+//Initialize game variables
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesSoFar = [];
+
+//Function to generate random computer guess
+function generateLetter(){
+    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log("Computer Guess: " + computerGuess)
+    return computerGuess;
+};
+
+//function to update guesses left on the DOM
+function updateGuesses(){
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+};
+
+// function to update the guessesSoFar array
+function lettersGuessed(){
+    guessesSoFar.push(userGuess);
+    document.getElementById("lettersGuessed").innerHTML = "Letters Guessed: " + guessesSoFar.toString();
+};
+
+
+
+
+
