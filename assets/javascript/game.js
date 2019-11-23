9721bf9c05089e2b1471764af1e8dcf1189ecff3
prpatel <prpatel1231@gmail.com>
2019-11-23 09:30:23 -0500

    added reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var letters = ["a","b","c","d","e","f","g",
                     "p","q","r","s","t","u","v","w",
                     "x","y","z"];
 
-//Initialize game variables
+//Initialize global game variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
@@ -13,29 +13,36 @@ var guessesSoFar = [];
 //Function to generate random computer guess
 function generateLetter(){
     computerGuess = letters[Math.floor(Math.random() * letters.length)];
-    console.log("Computer Guess: " + computerGuess)
+    console.log("Computer Guess: " + computerGuess);
     return computerGuess;
 };
 
 //function to update guesses left on the DOM
 function updateGuesses(){
     document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-    console.log(guessesLeft)
+    console.log(guessesLeft);
 };
 
 // function to update the guessesSoFar array
 function lettersGuessed(){
     guessesSoFar.push(userGuess);
     document.getElementById("lettersGuessed").innerHTML = "Letters Guessed: " + guessesSoFar.toString();
-    console.log(guessesSoFar)
+    console.log(guessesSoFar);
 };
 
 //function expression to generate the user's guess
 var userGuess = document.addEventListener("keydown",function(){
-    event.key
-    console.log(event.key)
+    event.key;
+    console.log(event.key);
 });
 
+//function that resets the game, to be used after a win or if guessesLeft = 0
+function resetGame (){
+    wins = 0;
+    losses = 0;
+    guessesLeft = 0;
+    guessesSoFar = [];
+};
 
 
 
